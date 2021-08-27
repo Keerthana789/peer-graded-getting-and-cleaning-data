66b29e6cb734ff51b7702df83e240c5622f21d33
Keerthana789 <86781942+Keerthana789@users.noreply.github.com>
2021-08-27 18:58:44 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,104 +1,90 @@
-#******************************************************************
-#Step 0. Downloading and unzipping dataset
-#******************************************************************
+## This script creates a tidy data set of UCI HAR Data Set
 
-if(!file.exists("./data")){dir.create("./data")}
-#Here are the data for the project:
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="./data/Dataset.zip")
+library(dplyr)
+library(stringr)
 
-# Unzip dataSet to /data directory
-unzip(zipfile="./data/Dataset.zip",exdir="./data")
+setwd("C:...\getting and cleaning data\\Assignement 4\\UCI HAR Dataset")
 
-#You should create one R script called run_analysis.R that does the following.
+# Read training data into a train file 
+trainFileName <- "train\\X_train.txt"
+trainFile<- read.table(trainFileName)
 
-#******************************************************************
-#Step 1.Merges the training and the test sets to create one data set.
-#******************************************************************
+# read test data into a test file 
+testFileName <- "test\\X_test.txt"
+testFile<- read.table(testFileName)
 
-# 1.1 Reading files
+# read training data labels 
+trainLabelFileName <- "train\\y_train.txt"
+trainLabelFile <- read.table(trainLabelFileName, colClasses = "character")
 
-# 1.1.1  Reading trainings tables:
+# read test data labels 
+testLabelFileName <- "test\\y_test.txt"
+testLabelFile <- read.table(testLabelFileName, colClasses = "character")
 
-x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+# read the train subject file 
+trainSubjectFileName <- "train\\subject_train.txt"
+trainSubjectFile <- read.table(trainSubjectFileName)
 
-# 1.1.2 Reading testing tables:
-x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-# 1.1.3 Reading feature vector:
-features <- read.table('./data/UCI HAR Dataset/features.txt')
+# read the test subject file 
+testSubjectFileName <- "test\\subject_test.txt"
+testSubjectFile <- read.table(testSubjectFileName)
+subjectFile <- rbind(trainSubjectFile,testSubjectFile)
 
-# 1.1.4 Reading activity labels:
-activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+# read the feature info file 
+featureFileName <- "features.txt"
+featureFile <- read.table(featureFileName,colClasses = c("numeric","character"),col.names = c("srNo","featureName"))
 
-# 1.2 Assigning column names:
+# read the activity label file 
+activityLabelFileName <- "activity_labels.txt"
+activityLabelFile <- read.table(activityLabelFileName,colClasses = c("numeric","character"),col.names = c("srNo", "activityName"))
 
-colnames(x_train) <- features[,2]
-colnames(y_train) <-"activityId"
-colnames(subject_train) <- "subjectId"
+trainLabelFile$V1 <- str_replace_all(trainLabelFile$V1,c("1"=activityLabelFile$activityName[1],
+                                  "2"=activityLabelFile$activityName[2],
+                                  "3"=activityLabelFile$activityName[3],
+                                  "4"=activityLabelFile$activityName[4],
+                                  "5"=activityLabelFile$activityName[5],
+                                  "6"=activityLabelFile$activityName[6]));
 
-colnames(x_test) <- features[,2] 
-colnames(y_test) <- "activityId"
-colnames(subject_test) <- "subjectId"
+testLabelFile$V1 <- str_replace_all(testLabelFile$V1,c("1"=activityLabelFile$activityName[1],
+                                                         "2"=activityLabelFile$activityName[2],
+                                                         "3"=activityLabelFile$activityName[3],
+                                                         "4"=activityLabelFile$activityName[4],
+                                                         "5"=activityLabelFile$activityName[5],
+                                                         "6"=activityLabelFile$activityName[6]));
 
-colnames(activityLabels) <- c('activityId','activityType')
+activityLabels <- rbind(trainLabelFile,testLabelFile)
+# Select the mean features
+meanFeatureIndices <- grep("mean", featureFile$featureName)
 
-#1.3 Merging all data in one set:
+# select the std features
+stdFeatureIndices <- grep("std", featureFile$featureName)
+selectedFeatureIndices <- c(meanFeatureIndices, stdFeatureIndices)
 
-mrg_train <- cbind(y_train, subject_train, x_train)
-mrg_test <- cbind(y_test, subject_test, x_test)
-setAllInOne <- rbind(mrg_train, mrg_test)
+# create cleaned vectors of these feature indices 
+meanFeatureNames <- featureFile[meanFeatureIndices,]
+stdFeatureNames <- featureFile[stdFeatureIndices,]
 
-#dim(setAllInOne)
-#[1] 10299   563
 
-#******************************************************************
-#Step 2.-Extracts only the measurements on the mean and standard deviation for each measurement.
-#******************************************************************
+# Clean the feature Names 
+meanFeatureNames$featureName <- gsub("-|\\(|\\)","",meanFeatureNames$featureName)
+stdFeatureNames$featureName <- gsub("-|\\(|\\)","",stdFeatureNames$featureName)
+cleanedFeatureNames <- rbind(meanFeatureNames, stdFeatureNames)
 
-#2.1 Reading column names:
+# select the sub set of columns form the test and train data set 
+targetTrainData <- trainFile[,selectedFeatureIndices]
+targetTestData <- testFile[,selectedFeatureIndices]
 
-colNames <- colnames(setAllInOne)
+targetDataSet <- rbind(targetTrainData,targetTestData)
 
-#2.2 Create vector for defining ID, mean and standard deviation:
+# Append the subject Id and activity Labels in the target data set
+targetDataSet <- cbind(subjectFile, activityLabels, targetDataSet)
 
-mean_and_std <- (grepl("activityId" , colNames) | 
-                   grepl("subjectId" , colNames) | 
-                   grepl("mean.." , colNames) | 
-                   grepl("std.." , colNames) 
-)
+# Assign the column names of the data frame 
+colnames(targetDataSet)<- c("subjectID","activityLabel",cleanedFeatureNames$featureName)
+write.table(targetDataSet, file = "HARTidyDataSet.")
 
-#2.3 Making nessesary subset from setAllInOne:
+averageDataset <- aggregate(. ~subjectID + activityLabel, targetDataSet, mean)
+averageDataset <- averageDataset[order(averageDataset$subjectID, averageDataset$activityLabel),]
 
-setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
-
-#******************************************************************
-#Step 3. Uses descriptive activity names to name the activities in the data set
-#******************************************************************
-
-setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
-                              by='activityId',
-                              all.x=TRUE)
-
-#******************************************************************
-#Step 4. Appropriately labels the data set with descriptive variable names.
-#******************************************************************
-
-#Done in previous steps, see 1.3,2.2 and 2.3!
-
-#******************************************************************
-#Step 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-#******************************************************************
-
-#5.1 Making a second tidy data set
-
-secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
-secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
-
-#5.2 Writing second tidy data set in txt file
-
-write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
+write.table(averageDataset, file = "averageDatasetBySubjectAndActivity.txt")
